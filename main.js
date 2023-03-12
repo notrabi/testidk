0962705648fc104fdaafad0d95caa96b4657a211
notrabi <127636988+notrabi@users.noreply.github.com>
2023-03-12 14:55:16 +0000

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-var data = {
-    Points: 0,
-    PointsPerClick: 1,
-}
-
-function getPoints() {
-    data.Points += data.PointsPerClick
-    document.ElementFindId("pointDisplay").innerhtml = data.Points + " Points"
-}
